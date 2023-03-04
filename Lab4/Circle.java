b306b0d7312d3803e00a2278e18f54dea960ccab
19w4cmm0 <125661366+19w4cmm0@users.noreply.github.com>
2023-03-04 16:49:36 +0700

    Add files via upload

---
+++ Lab4/Circle.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab4;
+
+/**
+ *
+ * @author Admin
+ */
+public class Circle {
+    private double radius;
+    private String color;
+    
+    public Circle(){
+        super();
+    }
+    
+    public Circle(double radius, String color){
+        super();
+        this.radius = radius;
+        this.color = color;
+        
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
