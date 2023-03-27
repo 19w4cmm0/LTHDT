54a4814651ed1413c22117a45e8fd666c925e4bc
19w4cmm0 <125661366+19w4cmm0@users.noreply.github.com>
2023-03-27 19:03:22 +0700

    Add files via upload

---
+++ Lab7/src/lab7/XeMayTheThao.java
@@ -0,0 +1,11 @@
+
+package lab7;
+
+public class XeMayTheThao extends XeMay {
+
+    @Override
+    void brake() {
+        System.out.println("Brake xe may the thao");
+    }
+    
+}
---
+++ Lab7/src/lab7/main.java
@@ -0,0 +1,12 @@
+
+package lab7;
+
+
+public class main {
+    public static void main(String[] args) {
+        XeMayLeoNui ln = new XeMayLeoNui();
+        XeMayTheThao tt = new XeMayTheThao();
+        ln.brake();
+        tt.brake();
+    }
+}
